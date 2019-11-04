543cef21ec2e0512d060673e58ee718f8fada243
rmflawk <ant921111@naver.com>
2019-11-04 17:22:32 +0900

    첫번째 커밋

---
+++ app/src/main/java/com/youngstudio/ex88preferencefragment/MainActivity.java
@@ -0,0 +1,18 @@
+package com.youngstudio.ex88preferencefragment;
+
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
